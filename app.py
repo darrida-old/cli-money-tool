4c7282e204edcfe5d433e00c6a78b5e8e9e6a679
darrida <darridapy@gmail.com>
2020-02-05 00:11:27 -0600

    update

---
+++ app.py
@@ -0,0 +1,28 @@
+# Standard Library
+import sqlite3
+
+# PYPI
+import click
+
+
+
+
+
+
+
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+    return conn
+
+
+conn = create_connection('accounts.db')
